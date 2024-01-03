6afeae0c4b525b276a35384c526d4d0f5aea41e5
vidyadhar03 <vidyadhariitkgp@gmail.com>
2024-01-03 23:15:30 +0530

    created final basic landing page structure of the site

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ function App() {
   console.log(projects);
 
   return (
-    <div className=" h-screen">
-      <div className="p-6 font-medium text-4xl">Vidyadhar Gowd J</div>
-      <div className="px-6 font-semibold">
+    <div className="bg-slate-200">
+      <div className="px-6 py-4 font-medium text-6xl ">Vidyadhar Gowd</div>
+
+      {/* <div className="px-6 font-semibold">
         Github:{" "}
         <a
           href="https://github.com/vidyadhar03"
@@ -15,20 +16,39 @@ function App() {
         >
           @vidyadhar03
         </a>
-        <div>
-          Contact: vidyadhariitkgp@gmail.com , +917478022333
+        <div>Contact: vidyadhariitkgp@gmail.com , +917478022333</div>
+      </div> */}
+
+      <div className="px-6 py-4 font-semibold text-xl w-96 break-words">
+        Senior Software Engineer with over 3 years of experience with passion to
+        develop useful websites for people.
+      </div>
+
+      <div className="flex rounded-xl shadow-lg mx-12 my-8">
+        <div className="flex-1 p-8 text-xl text-center font-medium border-gray-800 border-r-2 my-2">
+          Bajaj Finserv Ltd.
+        </div>
+        <div className="flex-1 relative">
+          <div className="absolute inset-0 flex items-center justify-cente text-lg text-center px-6">
+            Wroked on 3in1 App and managed 7 out of the 23 App modules and did
+            android development of store locator module using MMI Map SDK.
+          </div>
         </div>
       </div>
-      <div className="px-6 font-bold text-2xl mt-6">Projects</div>
-      <div className="grid grid-cols-3">
+
+      <div className="px-6 font-bold text-2xl mt-6">Full Stack Projects</div>
+
+      <div className="grid grid-cols-2">
         {projects.map((project, index) => (
-          <div key={index} className="">
-            <div className="flex flex-col p-4 border-2 border-black-200 rounded-md shadow-md m-4 bg-white">
-              <div className="mb-4 font-medium text-xl">{project.title}</div>
-              <div className="flex">
-                <div className="font-semibold mr-2">Tech Stack :</div>
-                <div className=""> {project.stack}</div>
-              </div>
+          <div
+            key={index}
+            className="flex flex-col justify-between p-4 border-2 rounded-xl shadow-lg my-4 mx-12 bg-blur"
+          >
+            <div className="mb-4 font-medium text-xl ">{project.title}</div>
+            <div className="mb-2 ">{project.description}</div>
+            <div className="flex ">
+              <div className="font-semibold mr-2">Tech Stack :</div>
+              <div className="">{project.stack}</div>
             </div>
           </div>
         ))}
--- src/Data.js
+++ src/Data.js
@@ -1,21 +1,26 @@
 export const projects=[{
     title:"Trippy Tree",
     stack:" React,Firebase for DB,Authentication & Hosting",
-    github:"https://github.com/vidyadhar03/TrippyTree"
+    github:"https://github.com/vidyadhar03/TrippyTree",
+    description:"An E-commerce site which is a collection of trip essentials."
 },{
     title:"Tracker",
     stack:"React,Express,MongoDB",
-    github:"https://github.com/vidyadhar03/Tracker"
+    github:"https://github.com/vidyadhar03/Tracker",
+    description:"A Site for mental wellbeing using simple and straight forward thought tracking method."
 },{
     title:"Pluto",
     stack:"React,Express,MongoDB",
-    github:"https://github.com/vidyadhar03/pluto-client"
+    github:"https://github.com/vidyadhar03/pluto-client",
+    description:"A trading platform coded from scratch in MERN stack."
 },{
     title:"LeetCode Clone",
     stack:"React,Express,MongoDB",
-    github:"https://github.com/vidyadhar03/LeetCode-Clone"
+    github:"https://github.com/vidyadhar03/LeetCode-Clone",
+    description:"Cloned leet-code, the platform to write and test code."
 },{
     title:"Flappy Bird",
     stack:"React,Express,MongoDB",
-    github:"https://github.com/hkirat/flappyBirdAi"
+    github:"https://github.com/hkirat/flappyBirdAi",
+    description:"Flappy bird game fed with genetic algorithm AI."
 },]
