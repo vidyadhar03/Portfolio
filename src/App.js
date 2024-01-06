86b7f9586f79577667f9eeb66fc22f76e7477b17
vidyadhar03 <vidyadhariitkgp@gmail.com>
2024-01-06 22:38:47 +0530

    revamped UI and made responsive

--- src/App.js
+++ src/App.js
@@ -1,53 +1,70 @@
 import { projects } from "./Data";
+import "./App.css";
 
 function App() {
   console.log(projects);
 
   return (
-    <div className="bg-slate-200">
-      <div className="px-6 py-4 font-medium text-6xl ">Vidyadhar Gowd</div>
+    <div class="parent">
+      <div className="font-medium text-6xl">Vidyadhar Gowd</div>
 
-      {/* <div className="px-6 font-semibold">
-        Github:{" "}
-        <a
-          href="https://github.com/vidyadhar03"
-          target="_blank"
-          className="hover:text-blue-800"
-        >
-          @vidyadhar03
-        </a>
-        <div>Contact: vidyadhariitkgp@gmail.com , +917478022333</div>
-      </div> */}
+      <div className="flex mt-4">
+        <div className="flex items-center">
+          <img src="./githublogo.png" className="w-8 h-8" />
+          <a
+            href="https://github.com/vidyadhar03"
+            target="_blank"
+            className="font-semibold underline hover:no-underline"
+          >
+            vidyadhar03
+          </a>
+        </div>
+        <div className="flex items-center ml-4 font-semibold underline hover:no-underline cursor-pointer">
+          Get in touch
+        </div>
+      </div>
 
-      <div className="px-6 py-4 font-semibold text-xl w-96 break-words">
+      <div class="designation">
         Senior Software Engineer with over 3 years of experience with passion to
         develop useful websites for people.
       </div>
 
-      <div className="flex rounded-xl shadow-lg mx-12 my-8">
-        <div className="flex-1 p-8 text-xl text-center font-medium border-gray-800 border-r-2 my-2">
-          Bajaj Finserv Ltd.
-        </div>
-        <div className="flex-1 relative">
-          <div className="absolute inset-0 flex items-center justify-cente text-lg text-center px-6">
-            Wroked on 3in1 App and managed 7 out of the 23 App modules and did
-            android development of store locator module using MMI Map SDK.
+      <div class="experiencediv">
+        <div class="exptitle">
+          <div className="cursor-pointer underline hover:no-underline ">
+            Bajaj Finserv Ltd.
           </div>
+          <div className="text-xl mt-1">2020-2023</div>
+        </div>
+        <div class="expdesc">
+          Wroked on 3in1 App and managed 7 out of the 23 App modules and did
+          android development of store locator module using MMI Map SDK.
+          <br />
+          Wroked on 3in1 App and managed 7 out of the 23 App modules and did
+          android development of store locator module using MMI Map SDK.
         </div>
       </div>
 
-      <div className="px-6 font-bold text-2xl mt-6">Full Stack Projects</div>
+      <div className="font-bold text-2xl mt-8 ">Projects</div>
 
-      <div className="grid grid-cols-2">
+      <div class="projectsdiv">
         {projects.map((project, index) => (
-          <div
-            key={index}
-            className="flex flex-col justify-between p-4 border-2 rounded-xl shadow-lg my-4 mx-12 bg-blur"
-          >
-            <div className="mb-4 font-medium text-xl ">{project.title}</div>
-            <div className="mb-2 ">{project.description}</div>
-            <div className="flex ">
-              <div className="font-semibold mr-2">Tech Stack :</div>
+          <div key={index} class="projectinddiv">
+            <div className="flex mb-4 ">
+              <div className="flex-1 font-bold text-2xl underline hover:no-underline cursor-pointer">{project.title}</div>
+              <div className="flex-1  flex justify-end">
+                <a
+                  href={project.github}
+                  target="_blank"
+                >
+                  <img src="./githublogo.png" className="w-8 h-8"/>
+                </a>
+              </div>
+            </div>
+            <div className="mb-2 text-lg">{project.description}</div>
+
+            <div className=" text-md">
+              <div className="font-semibold mt-2 ">Built With:</div>
               <div className="">{project.stack}</div>
             </div>
           </div>
