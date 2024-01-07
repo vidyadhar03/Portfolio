1572224544c8c9a1b13d4b7ad1879c33a3623029
vidyadhar03 <vidyadhariitkgp@gmail.com>
2024-01-08 03:27:44 +0530

    added info and site redirections

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { projects } from "./Data";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  console.log(projects);
+  const [showmail, setShowmail] = useState(false);
 
   return (
     <div class="parent">
       <div className="font-medium text-6xl">Vidyadhar Gowd</div>
 
-      <div className="flex mt-4">
+      <div class="info">
         <div className="flex items-center">
           <img src="./githublogo.png" className="w-8 h-8" />
           <a
@@ -19,8 +20,17 @@ function App() {
             vidyadhar03
           </a>
         </div>
-        <div className="flex items-center ml-4 font-semibold underline hover:no-underline cursor-pointer">
+        <div
+          class="touch"
+          onClick={() => {
+            showmail ? setShowmail(false) : setShowmail(true);
+          }}
+        >
+          <img src="./gmailogo.png" className="w-6 h-6 mx-1" />
           Get in touch
+        </div>
+        <div class="email">
+          {showmail && <div>vidyadhariitkgp@gmail.com</div>}
         </div>
       </div>
 
@@ -32,7 +42,9 @@ function App() {
       <div class="experiencediv">
         <div class="exptitle">
           <div className="cursor-pointer underline hover:no-underline ">
-            Bajaj Finserv Ltd.
+            <a href="https://www.bajajfinserv.in/" target="_blank">
+              Bajaj Finserv Ltd.
+            </a>
           </div>
           <div className="text-xl mt-1">2020-2023</div>
         </div>
@@ -51,13 +63,14 @@ function App() {
         {projects.map((project, index) => (
           <div key={index} class="projectinddiv">
             <div className="flex mb-4 ">
-              <div className="flex-1 font-bold text-2xl underline hover:no-underline cursor-pointer">{project.title}</div>
+              <div className="flex-1 font-bold text-2xl underline hover:no-underline cursor-pointer">
+                <a href={project.sitelink} target="_blank">
+                  {project.title}
+                </a>
+              </div>
               <div className="flex-1  flex justify-end">
-                <a
-                  href={project.github}
-                  target="_blank"
-                >
-                  <img src="./githublogo.png" className="w-8 h-8"/>
+                <a href={project.github} target="_blank">
+                  <img src="./githublogo.png" className="w-8 h-8" />
                 </a>
               </div>
             </div>
--- src/Data.js
+++ src/Data.js
@@ -2,25 +2,30 @@ export const projects=[{
     title:"Trippy Tree",
     stack:" React,Firebase for DB,Authentication & Hosting",
     github:"https://github.com/vidyadhar03/TrippyTree",
-    description:"An E-commerce site which is a collection of trip essentials."
+    description:"An E-commerce site which is a collection of trip essentials.coded from scrath using reactjs and used firebase for the development purposes.",
+    sitelink:"https://google.com"
 },{
     title:"Tracker",
     stack:"React,Express,MongoDB",
     github:"https://github.com/vidyadhar03/Tracker",
-    description:"A Site for mental wellbeing using simple and straight forward thought tracking method."
+    description:"A Site for mental wellbeing using simple and straight forward thought tracking method. Need to integrate OPEN AI that is remaining yet to be completed!!",
+    sitelink:"https://google.com"
 },{
     title:"Pluto",
     stack:"React,Express,MongoDB",
     github:"https://github.com/vidyadhar03/pluto-client",
-    description:"A trading platform coded from scratch in MERN stack."
+    description:"A trading platform coded from scratch in MERN stack.",
+    sitelink:"https://google.com"
 },{
     title:"LeetCode Clone",
     stack:"React,Express,MongoDB",
     github:"https://github.com/vidyadhar03/LeetCode-Clone",
-    description:"Cloned leet-code, the platform to write and test code."
+    description:"Cloned leet-code, the platform to write and test code.",
+    sitelink:"https://google.com"
 },{
     title:"Flappy Bird",
     stack:"React,Express,MongoDB",
     github:"https://github.com/hkirat/flappyBirdAi",
-    description:"Flappy bird game fed with genetic algorithm AI."
+    description:"Flappy bird game fed with genetic algorithm AI.",
+    sitelink:"https://google.com"
 },]
