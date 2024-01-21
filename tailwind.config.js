ad3a552819280fac4e477f8dd7703d5010939540
vidyadhar03 <vidyadhariitkgp@gmail.com>
2024-01-21 10:09:09 +0530

    font and logo added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
