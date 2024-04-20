724c9745a87fc6b6c0859409cf9ea75a28dab816
vidyadhar03 <vidyadhariitkgp@gmail.com>
2024-04-21 01:22:08 +0530

    added stream mingle

--- src/Data.js
+++ src/Data.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: "Mind Cache AI",
-    stack: ["React", "Express", "Mongo DB","Razor Pay"],
+    stack: ["ReactJS", "Express", "Mongo DB", "Razor Pay"],
     github: "https://github.com/vidyadhar03/Tracker",
     description:
       "Mind Cache AI is a subscription-based service enhancing productivity with OpenAI GPT's AI capabilities. It includes an AI-powered Reflection Companion, incorporating React.js, Express.js, and MongoDB for a seamless user experience.",
@@ -9,32 +9,34 @@ export const projects = [
   },
   {
     title: "Web Weave",
-    stack: ["ReactJS", "Express","Redis","AWS S3"],
+    stack: ["ReactJS", "Express", "Redis", "AWS S3"],
     github: "https://github.com/vidyadhar03/WebWeave",
     description:
       "A platform that enables deployment of React and JavaScript front-ends from GitHub repositories URL directly. Utilizing a microservices architecture, it streamlines the deployment workflow and manages requests efficiently.",
-    sitelink: "https://www.google.co.in/",
+    sitelink: "https://github.com/vidyadhar03/WebWeave",
+  },
+  {
+    title: "Stream Mingle",
+    stack: ["NextJs", "PeerJS", "WebRTC", "Socket.IO"],
+    github: "https://github.com/vidyadhar03/Stream-Mingle",
+    description:
+      "A video conferencing platform using Next.js, enabling dynamic room creation and access through unique room IDs. Integrated PeerJS with WebRTC using mesh topology to manage direct multi-peer audio and video streams.",
+    sitelink: "https://github.com/vidyadhar03/Stream-Mingle",
   },
   {
     title: "Trippy Tree",
-    stack: ["React", "Firebase"],
+    stack: ["ReactJS", "Firebase"],
     github: "https://github.com/vidyadhar03/TrippyTree",
     description:
       "Trippy Tree is a comprehensive eCommerce site, crafted using Firebase and React.js. It offers a fluid and engaging online shopping experience, integrating modern web technologies for ease of use.",
     sitelink: "https://trippytree-a4793.web.app/",
   },
   {
-    title: "Neat Code",
-    stack: ["React", "Express", "Mongo DB"],
-    github: "https://github.com/vidyadhar03/Neat-Code",
-    description: "Neat Code, akin to LeetCode, is a platform for coding practice and collaboration, built with React.js and Express.js. It provides an interactive environment for learning and sharing among coding enthusiasts",
-    sitelink: "https://google.com",
-  },
-  {
     title: "Thought Tracker",
     stack: ["Android Studio", "Kotlin"],
     github: "https://github.com/vidyadhar03/ThoughtTracker",
-    description: "Thought Tracker is a minimalist Android app designed for introspection and personal reflection. It offers a straightforward and user-friendly platform for users to record and track their thoughts, aiding in self-awareness and mindfulness.",
+    description:
+      "Thought Tracker is a minimalist Android app designed for introspection and personal reflection. It offers a straightforward and user-friendly platform for users to record and track their thoughts, aiding in self-awareness and mindfulness.",
     sitelink:
       "https://play.google.com/store/apps/details?id=com.beinglife.thoughttracker&pcampaignid=web_share",
   },
